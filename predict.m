43b2c8e02db389e291df4374728bf30b3abd9de0
rohanv11 <rohanvaradkar1110@gmail.com>
2022-03-12 16:05:59 +0530

    forward propagation using 3 layer Neural Network
    Neural Network prediction implementation

--- predict.m
+++ predict.m
@@ -10,6 +10,10 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
+% Adding ones
+
+X = [ones(m,1) X]
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned neural network. You should set p to a 
@@ -22,8 +26,27 @@ p = zeros(size(X, 1), 1);
 %
 
 
+% totol 3 layers
+% 1 input 400 nurons
+% 2 hidden 25 neurons
+% 3 output 10 neurons
+
+% X = m * (n + 1) = m * 401 ; n = 400 ..added ones above
+% theta1 = 25 by 401
+% theta2 = 10 by 26
 
 
+  
+layer2 = X * Theta1'   % m * 25
+layer2 = sigmoid(layer2)    % m * 25
+layer2 = [ones(m, 1) layer2]    % m * 26
+
+layer3_output = layer2 * Theta2'   % m * 10
+layer3_output = sigmoid(layer3_output) % m * 10
+
+[values, indices] = max(layer3_output, [], 2)
+
+p = indices % m by 1
 
 
 
